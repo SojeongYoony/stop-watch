9d31e89b2f2f4ab81135f05e7cded33bf5767b91
SoJeongYoon <y940425@gmail.com>
2022-04-14 23:31:11 +0900

    Update SW-animationFrame.js

--- SW-animationFrame.js
+++ SW-animationFrame.js
@@ -1,4 +1,10 @@
 /*
-window.requestAnimationFrame 
+window.requestAnimationFrame
+문법     : window.requestAnimationFrame(callback);
+파라미터 : callback
+반환 값  : 고유한 요청 id인 long 정수 값. 
+           0이 아니라는 것 외에는 추측 할 수 없음.
+           이 값을 window.cancelAnimationFrame()에 전달하여 콜백 취소할 수 있음.
+
 window.cancelAnimationFrame
 */
